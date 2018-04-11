40e8e83ee5f3c87dea0fc740817469014832c75c
Franke <Lucas.Franke@haw-hamburg.de>
2018-04-11 11:59:42 +0200

    Bitrate Calc

--- server.js
+++ server.js
@@ -12,3 +12,33 @@ app.listen(port, function() {
 	console.log('listening to port ' + port);
 });
 
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/calculate', function(req, res){
+	// Werte aus Formular holen
+	const bitrate = parseFloat(req.body["bitrate"]);
+	const duration = parseFloat(req.body["duration"]);
+	
+	// einfaches Error-Handling, prüfen, ob die Eingabne gültig sind
+	// besserer Weg: siehe express-validator 
+	// https://github.com/ctavan/express-validator
+	if(isNaN(bitrate) || isNaN(duration)){
+		
+		res.render('error', {
+			'bitrate': req.body["bitrate"],
+			'duration': req.body["duration"]
+		});
+	}
+	
+	else {
+		const result = bitrate * duration;
+		res.render('result', {
+			'bitrate': bitrate,
+			'duration': duration,
+			'result': result
+		});
+	}
+});
